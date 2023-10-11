e3a250bfd87488cf179c0a8074fd143dc8fbe304
zeynallow <zeynaloffnet@gmail.com>
2023-10-11 16:53:09 +0400

    init

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
+    
+    private def render_not_found_response
+      render json: { error: "Post Not Found" }, status: :not_found
+    end
+  
+    def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def index
+        render json: Post.all
+    end
+
+    def show
+        render json: Post.find(params[:id])
+    end
+
+    # def create
+    #     post = Post.create!(artist_params)
+    #     render json: post, status: :created
+    #   end
+      
+      
+    #  private def posts_params
+    #     params.permit(:title, :content, :genre)
+    #   end
+
+    # def update
+    #     post = Post.find(params[:id])
+    #     post.update!(posts_params)
+    #     render json: post
+    #   end
+
+    # def destroy
+    #     Post.find(params[:id]).destroy
+    #     head :no_content
+    #   end
+
+end
